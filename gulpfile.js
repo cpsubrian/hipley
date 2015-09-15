9141ffebe983976a9eda06b4787f0f6d233cba6a
Brian Link <cpsubrian@gmail.com>
2015-09-15 15:43:02 -0700

    Gets production build working.

---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+var path = require('path')
+var spawn = require('child_process').spawn
+var _ = require('lodash')
+var rimraf = require('rimraf')
+var gulp = require('gulp')
+var gulpUtil = require('gulp-util')
+var less = require('gulp-less')
+var autoprefixer = require('gulp-autoprefixer')
+var plumber = require('gulp-plumber')
+var runSequence = require('run-sequence')
+var sourcemaps = require('gulp-sourcemaps')
+var webpack = require('webpack')
+var webpackconf = require('./webpack.config')
+var BrowserSync = require('browser-sync')
+var browserSync
+
+var ROOT = process.env.__root
+var SRC = path.resolve(ROOT, 'src')
+var BUILD = path.resolve(ROOT, 'build')
+var BS_PORT = 3000
+var MOT_PORT = 3003
+
+// Clean ./build folder.
+gulp.task('clean:build', function (cb) {
+  rimraf(BUILD, cb)
+})
+
+// Copy ./public assets.
+gulp.task('copy:public', function () {
+  var stream = gulp
+    .src(SRC + '/public/**/*')
+    .pipe(gulp.dest(BUILD))
+
+  if (browserSync) {
+    stream = stream.pipe(browserSync.stream({match: ['**/images/*']}))
+  }
+
+  return stream
+})
+
+// Initialize browserSync.
+gulp.task('browser-sync', function () {
+  browserSync = BrowserSync.create()
+  browserSync.init({
+    port: BS_PORT,
+    proxy: 'http://localhost:' + MOT_PORT,
+    open: false,
+    notify: false,
+    files: [
+      BUILD + '/**/*.html'
+    ]
+  })
+})
+
+// Compile LESS stylesheets.
+gulp.task('build:less', function () {
+  var stream = gulp.src(SRC + '/styles/*.less')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.error(err)
+        this.emit('end')
+      }
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(less())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(BUILD + '/css'))
+
+  if (browserSync) {
+    stream = stream.pipe(browserSync.stream({match: '**/*.css'}))
+  }
+
+  return stream
+})
+
+// Webpack production build.
+gulp.task('webpack:build', ['build'], function (cb) {
+  var conf = webpackconf({production: true})
+
+  webpack(conf, function (err, stats) {
+    if (err) throw new gulpUtil.PluginError('webpack:build', err)
+    gulpUtil.log('[webpack:build]', stats.toString({
+      colors: true
+    }))
+    cb()
+  })
+})
+
+// Watch ./public for changes.
+gulp.task('watch:public', function () {
+  gulp.watch(SRC + '/public/**/*', {interval: 2000}, ['copy:public'])
+})
+
+// Watch ./styles for changes.
+gulp.task('watch:styles', function () {
+  gulp.watch(SRC + '/styles/**/*', {interval: 300}, ['build:less'])
+})
+
+// Watch all the things.
+gulp.task('watch', ['watch:public', 'watch:styles'])
+
+// General build.
+gulp.task('build', function (cb) {
+  runSequence('clean:build', ['copy:public', 'build:less'], cb)
+})
+
+// Production
+gulp.task('build-prod', ['webpack:build'])
+
+// Default (Development)
+gulp.task('default', function (cb) {
+  runSequence('build', 'browser-sync', 'watch', cb)
+})
---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+var fs = require('fs')
+var path = require('path')
+var webpack = require('webpack')
+
+var ROOT = process.env.__root
+var PKG = require(path.resolve(ROOT, 'package.json'))
+var SRC = path.resolve(ROOT, 'src/js')
+var BUILD = path.resolve(ROOT, 'build')
+
+// Try to read .hipleyrc.
+var settings = {}
+try {
+  settings = JSON.parse(fs.readFileSync(path.resolve(ROOT, '.hipleyrc')))
+} catch (e) {}
+
+module.exports = function (options) {
+  // Initialize conf.
+  var conf = {
+    cache: true,
+    context: SRC,
+    entry: {
+      app: [
+        './app'
+      ],
+      vendors: settings.vendors || []
+    },
+    output: {
+      path: BUILD,
+      filename: 'js/app.js',
+      publicPath: '/'
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    },
+    resolveLoader: {
+      root: path.resolve(__dirname, 'node_modules')
+    },
+    module: {
+      loaders: [
+        { test: /\.js?$/, loaders: ['babel'], include: SRC }
+      ]
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin(),
+      new webpack.optimize.CommonsChunkPlugin('vendors', 'js/vendors.js')
+    ]
+  }
+
+  // Set root.
+  conf.root = SRC
+
+  // Enable sourcemaps.
+  conf.devtool = 'sourcemap'
+
+  // Production.
+  if (options.production) {
+    conf.plugins = conf.plugins.concat(
+      new webpack.DefinePlugin({
+        'process.env': {
+          'BROWSER': JSON.stringify(true),
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(true),
+      new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+    )
+  }
+
+  // Development Sever.
+  if (options.development) {
+    conf.debug = true
+    conf.entry.app = conf.entry.app.concat([
+      'webpack-hot-middleware/client'
+    ])
+    conf.plugins = conf.plugins.concat([
+      new webpack.DefinePlugin({
+        'process.env': {
+          'BROWSER': JSON.stringify(true),
+          'NODE_ENV': JSON.stringify('development')
+        }
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ])
+  }
+
+  return conf
+}
