447502fb7ac0e422bf351f8eee58f492106f96a5
Brian Link <cpsubrian@gmail.com>
2015-09-21 17:39:37 -0700

    Commits missing devServer.js.

---
+++ devServer.js
@@ -0,0 +1,57 @@
+var gulpUtil = require('gulp-util')
+var path = require('path')
+var webpack = require('webpack')
+var config = require('./webpack.config')({
+  development: true
+})
+var express = require('express')
+var compress = require('compression')
+var app = express()
+var compiler = webpack(config)
+var hipley = require('./')
+
+var ROOT = hipley.root
+var DEST = path.resolve(ROOT, hipley.options.dest)
+var STATIC = path.resolve(ROOT, hipley.options.static)
+var PORT = hipley.options.devServer
+var PROXY = hipley.options.proxy
+
+// Enable gzip compression.
+app.use(compress())
+
+// If we're not proxying, serve the dest and static directories.
+if (!PROXY) {
+  app.use(express.static(DEST))
+  app.use(express.static(STATIC))
+}
+
+// Catch requests for webpack resources.
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
+app.use(require('webpack-hot-middleware')(compiler))
+
+if (PROXY) {
+  // Proxy requests if needed.
+  var proxy = require('http-proxy').createProxyServer()
+  app.use(function (req, res) {
+    proxy.web(req, res, {target: 'http://localhost:' + PROXY}, function (err) {
+      console.error(err)
+    })
+  })
+} else {
+  // Otherwise, serve all requests with an index.html in the static directory.
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(STATIC, 'index.html'))
+  })
+}
+
+// Start listening.
+app.listen(PORT, 'localhost', function (err) {
+  if (err) {
+    gulpUtil.log(err)
+    return
+  }
+  gulpUtil.log('Development server listening at http://localhost:' + PORT)
+})
